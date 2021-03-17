b719b11177ed268c42a6aa0b3258f4076ca0e7f3
Diego Ferreira <diegoferreira1964@gmail.com>
2021-03-17 19:36:40 -0300

    Refatoracao do codigo e adcionando novas funcionalidades

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 /* eslint-disable prettier/prettier */
 // eslint-disable-next-line prettier/prettier
 
+// eslint-disable-next-line import-helpers/order-imports
 import express from "express";
-
-import { categoriesRoutes } from "./routes/categories.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
